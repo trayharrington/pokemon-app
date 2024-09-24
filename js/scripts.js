565815823f438e56dbea317a1668e5b7a8931033
Tray Harrington <traylon.harrington@gmail.com>
2024-09-24 15:07:24 -0500

    Created initial data array and added a few data objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
-let favoriteFood = 'pizza';
-document.write('My favorite food is ' + favoriteFood);
+// Create an array to store pokemon data
+pokemonList = [];
 
-alert('Hello world');
+// Add pokemon to the created array
+pokemonList.push({
+  name: Machop,
+  type: [fighting],
+  height: 0.8,
+  weight: 19.5
+})
+
+pokemonList.push({
+  name: Litwick,
+  type: [fire, ghost],
+  height: 0.3,
+  weight: 3.1
+})
+
+pokemonList.push({
+  name: Houndour,
+  type: [dark, fire],
+  height: 0.6,
+  weight: 10.8 
+})
+
+pokemonList.push({
+  name: Croagunk,
+  type: [poison, fighting],
+  height: 0.7,
+  weight: 23
+})
